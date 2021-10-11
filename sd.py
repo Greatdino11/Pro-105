b23e285555c9cad7432d3aaafc3a87248c75bedd
Greatdino11 <66023753+Greatdino11@users.noreply.github.com>
2021-10-10 22:02:51 -0500

    Add files via upload

---
+++ sd.py
@@ -0,0 +1,54 @@
+import csv
+import plotly.express as px
+import pandas as pd
+import math
+
+with open('data.csv', newline='') as f:
+    f_read = csv.reader(f)
+    data = list(f_read)
+marks = data[0]
+
+def mean(data):
+    sum = 0
+    n = len(data)
+    for i in marks:
+        sum += int(i)
+    mean = sum/n
+    return mean
+
+sq_l = []
+
+for i in marks:
+    a = int(i)- mean(marks)
+    a = a**2
+    sq_l.append(a)
+
+sum_s = 0
+
+for i in sq_l:
+    sum_s += i
+
+result = sum_s/(len(marks)-1)
+
+sd = math.sqrt(result)
+
+print(result, sd)
+
+
+'''with open('class_1.csv', newline='') as f:
+    f_read = csv.reader(f)
+    data = list(f_read)
+data.pop(0)
+
+sum = 0
+n = len(data)
+for i in data:
+    sum += float(i[1])
+mean = sum/n
+
+print('The mean score is', str(mean),'%')
+
+df = pd.read_csv('class_2.csv')
+scatter = px.scatter(df, x='Student Number', y="Marks")
+
+scatter.show()'''
